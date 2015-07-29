e1b675cb126674136347727030e8842eede06639
Robin Winslow <robin@robinwinslow.co.uk>
2015-07-29 19:55:17 +0100

    Make aggregator.py executable

--- aggregator.py
+++ aggregator.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +8,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
