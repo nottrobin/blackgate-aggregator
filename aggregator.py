0c540591c442cd88f2860c8f720438218836a9dd
Robin Winslow <robin@robinwinslow.co.uk>
2015-07-29 22:24:21 +0100

    Read raw RSS feed

--- aggregator.py
+++ aggregator.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
+# Modules
 from flask import Flask
+import requests
+import requests_cache
+
+requests_cache.install_cache('aggregator.cache')
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    rss_request = requests.get('https://robinwinslow.co.uk/rss-canonical.xml', verify=False)
+    return rss_request.content
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
