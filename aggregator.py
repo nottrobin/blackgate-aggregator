53ea925fb1cd44f30e267cb01d6f00c9f934b0b8
Robin Winslow <robin@robinwinslow.co.uk>
2015-07-29 22:37:38 +0100

    Basic feed output

--- aggregator.py
+++ aggregator.py
@@ -2,6 +2,7 @@
 
 # Modules
 from flask import Flask
+import feedparser
 import requests
 import requests_cache
 
@@ -12,7 +13,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     rss_request = requests.get('https://robinwinslow.co.uk/rss-canonical.xml', verify=False)
-    return rss_request.content
+    rss = feedparser.parse(rss_request.content)
+
+    body = '<ul>'
+
+    for article in rss['entries']:
+        body += "<li>{}: {}</li>".format(article['updated'], article['title'])
+
+    return body + '</ul>'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
